342c9ce93051060e4e2d7c66a73c02bb45d827f4
张金山 <2649154231@qq.com>
2018-02-25 08:31:11 +0800

    no message

---
+++ WebViewBridgeDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  WebViewBridgeDemo
+//
+//  Created by 张金山 on 16/3/9.
+//  Copyright © 2016年 张金山. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ WebViewBridgeDemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  WebViewBridgeDemo
+//
+//  Created by 张金山 on 16/3/9.
+//  Copyright © 2016年 张金山. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
